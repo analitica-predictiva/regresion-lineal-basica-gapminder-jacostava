3d9e27a872258c235529c32e2ba6f810b3c99fd5
Juan David Acosta <jacostava@unal.edu.co>
2023-11-06 16:39:48 -0500

    :sparkles: jacostava: Lab 2

--- preguntas.py
+++ preguntas.py
@@ -117,7 +117,7 @@ def pregunta_04():
     from sklearn.metrics import mean_squared_error
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    reg = LinearRegression()
+    df = pd.read_csv("gm_2008_region.csv")
 
     # Asigne a la variable los valores de la columna `fertility`
     X_fertility = df["fertility"].array.reshape(-1,1)
