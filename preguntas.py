02e1bf75038f291e650cb52fc01eb2ce49fa8a33
Juan David Acosta <jacostava@unal.edu.co>
2023-11-08 18:38:50 -0500

    :sparkles: jacostava: Lab 2

--- preguntas.py
+++ preguntas.py
@@ -53,7 +53,7 @@ def pregunta_02():
     print(df.shape)
 
     # Imprima la correlación entre las columnas `life` y `fertility` con 4 decimales.
-    print(df["life"].corr(df["fertility"]))
+    print(df["life"].corr(df["fertility"]).round(4))
 
     # Imprima la media de la columna `life` con 4 decimales.
     print(df["life"].mean().round(4))
